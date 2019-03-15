68e6828cb0a2ca7594d6a0bb4629d3fa7f57a871
Maris Orbidans <maris.orbidans@onthemarket.com>
2019-03-15 01:19:41 +0000

    cleanup test

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,9 +12,9 @@ listOfHeight :: Gen [Int]
 listOfHeight = listOf genHeight
 
 main :: IO ()
-main = hspec $ do
+main = hspec $ 
   describe "largest rectangle" $ do
-    it "returns all possible subsequences" $ do
+    it "returns all possible subsequences" $ 
       sort (subs "1234") `shouldBe` ["1","12","123","1234","2","23","234","3","34","4"]
 
     it "returns size of the largest rectangle" $ do
@@ -23,8 +23,8 @@ main = hspec $ do
       largestRectangle [4,4,23,21,13,20,22,6,8,8,4,16,14,6] `shouldBe` 65
       largestRectangle [6,14,16,4,8,8,6,22,20,13,21,23,4,4] `shouldBe` 65
 
-    it "result is equal to the naive solution" $ do
+    it "result is equal to the naive solution" $ 
       forAll listOfHeight $ \xs -> largestRectangle xs == naiveSolution xs
 
-    it "result should be the same if list is reversed" $ do
+    it "result should be the same if list is reversed" $ 
       forAll listOfHeight $ \xs -> largestRectangle xs == largestRectangle (reverse xs)
